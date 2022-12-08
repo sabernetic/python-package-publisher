a8987a4dd2f368852f713325088cd2cfb12a3e92
trithai1 <trithai45@gmail.com>
2022-12-08 16:13:29 -0600

    Update publisher.sh

--- publisher.sh
+++ publisher.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-pip install twine wheel
+# pip install twine wheel
+# python setup.py sdist bdist_wheel
+# export token=$(aws codeartifact get-authorization-token --domain sabernetic --domain-owner 538326023882 --query authorizationToken --output text)
+# twine upload --repository codeartifact dist/* -u aws -p $token
+
+export TWINE_USERNAME=aws
+export TWINE_PASSWORD=`aws codeartifact get-authorization-token --domain $1 --domain-owner $3 --query authorizationToken --output text`
+export TWINE_REPOSITORY_URL=`aws codeartifact get-repository-endpoint --domain $1 --domain-owner $3 --repository $2 --format pypi --query repositoryEndpoint --output text`
 python setup.py sdist bdist_wheel
-export token=$(aws codeartifact get-authorization-token --domain sabernetic --domain-owner 538326023882 --query authorizationToken --output text)
-twine upload --repository codeartifact dist/* -u aws -p $token
+twine upload dist/*
