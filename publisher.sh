da3e947b38641edc904e685cd3c49ef2363f6e22
trithai1 <trithai45@gmail.com>
2022-12-12 13:32:37 -0600

    Update publisher.sh

--- publisher.sh
+++ publisher.sh
@@ -7,5 +7,4 @@ export TWINE_USERNAME=aws
 export TWINE_PASSWORD=`aws codeartifact get-authorization-token --domain $domain --domain-owner $acc_id --query authorizationToken --output text`
 export TWINE_REPOSITORY_URL=`aws codeartifact get-repository-endpoint --domain $domain --domain-owner $acc_id --repository $repo --format pypi --query repositoryEndpoint --output text`
 python setup.py sdist bdist_wheel
-ls -a
 twine upload dist/*
