c843e055aadef7a7c3bc1bf1758fd21fd9c1e747
trithai1 <trithai45@gmail.com>
2023-01-04 12:14:32 -0600

    Update publisher.sh

--- publisher.sh
+++ publisher.sh
@@ -3,7 +3,7 @@ acc_id=$1
 domain=$2
 repo=$3
 
-conda install setuptools wheel twine
+pip install setuptools wheel twine
 export TWINE_USERNAME=aws
 export TWINE_PASSWORD=`aws codeartifact get-authorization-token --domain $domain --domain-owner $acc_id --query authorizationToken --output text`
 export TWINE_REPOSITORY_URL=`aws codeartifact get-repository-endpoint --domain $domain --domain-owner $acc_id --repository $repo --format pypi --query repositoryEndpoint --output text`
